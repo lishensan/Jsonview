40e2fc79c4ce8aa2db4079ec572bd7b319918527
shenshan <lishenshan@gmail.com>
2018-05-30 09:20:21 +0800

    Revert "test"
    This reverts commit 7005bd2dd815b953f9c2bd40bbe57bbcf5b990d6.

--- app/src/main/java/com/mountain/jsview/ImageLoader.java
+++ app/src/main/java/com/mountain/jsview/ImageLoader.java
@@ -6,7 +6,6 @@ import com.bumptech.glide.Glide;
 
 public class ImageLoader {
     public static void loadImage(ImageView imageView, String url) {
-
         Glide.with(imageView.getContext())
                 .load(url)
                 .into(imageView);
